b4c8e7052627eb06bce6fd178dd8cf24821bbe49
Dana <danaahmadi2014@gmail.com>
2025-11-12 19:32:36 +0800

    Add map component
    - scaffold map component.
    - remove extra styling and apply correct style to body of the application.

--- truck-tracker/src/app/app.component.ts
+++ truck-tracker/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Map } from "src/map/map";
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, Map],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
