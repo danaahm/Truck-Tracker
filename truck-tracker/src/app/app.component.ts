8b26f2a8983b93830a5389f048e956b0b563fdf0
Dana <danaahmadi2014@gmail.com>
2025-11-12 19:23:25 +0800

    Upgrade packages
    - Fix the issue of app import in latest version.
    - Upgrade the dependencies

--- truck-tracker/src/app/app.component.ts
+++ truck-tracker/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'truck-tracker';
 }
+
