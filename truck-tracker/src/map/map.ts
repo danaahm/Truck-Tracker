61c819219efd8fcc829bfa6dc1afb5703bddcb36
Dana <danaahmadi2014@gmail.com>
2025-11-13 11:03:27 +0800

    Clean up map

--- truck-tracker/src/map/map.ts
+++ truck-tracker/src/map/map.ts
@@ -9,31 +9,23 @@ import { TruckService } from 'src/services/truck.service';
   styleUrl: './map.scss',
 })
 export class Map implements OnInit {
-
   truck: Truck | null = null;
 
-  constructor(
-    private truckService: TruckService,
-  ) {}
+  constructor(private truckService: TruckService) {}
 
   ngOnInit(): void {
     this.truckService.truck$.subscribe((truck) => {
       this.truck = truck;
       this.update();
-    })
+    });
   }
 
   update() {
     const truckElement = document.getElementById('truck');
-    if(this.truck && truckElement) {
+    if (this.truck && truckElement) {
       truckElement.style.transform = `translate(${this.truck.position.x}px, ${this.truck.position.y}px)`;
-      truckElement.style.backgroundColor = TruckStatusColours[this.truck.status];
-      truckElement.setAttribute('data-status', this.truck.status);
-      truckElement.setAttribute('data-speed', `${this.truck.speed} km/h`);
+      truckElement.style.backgroundColor =
+        TruckStatusColours[this.truck.status];
     }
-
   }
-
-
-
 }
